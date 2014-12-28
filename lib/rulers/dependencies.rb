e72600e3dcdb3c3c07385a1f6fe4a08aeac15959
zaiquiri <zachary.lindberg@gmail.com>
2014-12-28 18:53:14 -0500

    Fixed dependencies

--- lib/rulers/dependencies.rb
+++ lib/rulers/dependencies.rb
@@ -1,10 +1,6 @@
 class Object
   def self.const_missing(c)
-    return nil if @calling_const_missing
-
-    @calling_const_missing = true
     require Rulers.to_underscore(c.to_s)
     Object.const_get(c)
-    @calling_const_missing = false
   end
 end
